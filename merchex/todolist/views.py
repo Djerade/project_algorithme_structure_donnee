480036b26ca08b01bd7c62c87e23d43c6e04284e
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-04 13:37:23 +0000

    get data from DB

--- merchex/todolist/views.py
+++ merchex/todolist/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from todolist.models import Task
+
 def home(request):
-    template = loader.get_template('index.html')
-    return HttpResponse("<> bonojour</>")
+    tasks = Task.objects.all()
+    print(tasks)
+    # template = loader.get_template('index.html')
+    return HttpResponse(f"""
+                         <h1>Task !</h1>
+                         <p>Nos tasks de la journée<p>
+                         <ul>
+                             <li>{tasks[0].name_task}</li>
+                          </ul>
+                        """)
