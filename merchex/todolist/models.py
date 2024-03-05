929578f48de58c87c2629eb73b43d122c7603ad8
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-05 16:50:23 +0000

    remove attribut task

--- merchex/todolist/models.py
+++ merchex/todolist/models.py
@@ -3,11 +3,3 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     name_task = models.fields.CharField(max_length=100)
-    description_task = models.fields.CharField(max_length=500)
-    priority_task = models.fields.CharField(max_length=100)
-    date_task = models.fields.CharField(max_length=100)
-    category_task = models.fields.CharField(max_length=100)
-    
-    
-    def __str__(self) -> str:
-        return self.name_task
