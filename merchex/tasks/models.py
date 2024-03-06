52314e073342fe338ad46011d758bd53dd6de180
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-06 22:05:18 +0000

    update

--- merchex/tasks/models.py
+++ merchex/tasks/models.py
@@ -15,11 +15,11 @@ class Category(models.TextChoices):
     SOCIAL = 's', "Social"
 
 class Task(models.Model):
-    title = models.CharField(verbose_name="Task name", max_length=65, unique=True)
-    description = models.CharField(verbose_name="Task description", max_length=100)
+    title = models.CharField(verbose_name="titre", max_length=65, unique=True)
+    description = models.CharField(verbose_name="description", max_length=100)
     priority = models.CharField(verbose_name="priorité", max_length=1, choices=Priority.choices)
     category = models.CharField(verbose_name="Categorie", max_length=5, choices=Category.choices)
     date = models.CharField(verbose_name="Date d'écheance", max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.title
