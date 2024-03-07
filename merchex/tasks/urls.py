69f8577f4f9e8259e6396b48081aecfa6e000b77
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-07 10:55:21 +0000

    update

--- merchex/tasks/urls.py
+++ merchex/tasks/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     re_path(r'^(?P<pk>\d+)/$', views.TaskDetailView.as_view(), name='task_detail'),
     re_path(r'^(?P<pk>\d+)/update/$', views.TaskUpdateView.as_view(), name='task_update'),
     re_path(r'^(?P<pk>\d+)/delete/$', views.TaskDeleteView.as_view(), name='task_delete')
-
-
 ]
--- merchex/tasks/views.py
+++ merchex/tasks/views.py
@@ -4,7 +4,6 @@ from .models import Task
 from .forms import TaskForm
 
 
-# Class Based Views
 from django.views.generic import ListView, DetailView, \
     CreateView, UpdateView, DeleteView
 
