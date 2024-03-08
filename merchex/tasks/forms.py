93038817ed615c80c628f329774ba88ece0f353f
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-08 15:23:20 +0000

    update

--- merchex/tasks/forms.py
+++ merchex/tasks/forms.py
@@ -8,11 +8,3 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = "__all__"
 
-
-
-# class  TaskForm(forms.Form):
-#     title = forms.CharField(label="titre", max_length=65)
-#     description = forms.CharField(label="description", max_length=100)
-#     priority = forms.CharField(label="priorité", max_length=1)
-#     category = forms.CharField(label="Categorie", max_length=5)
-#     date = forms.CharField(label="Date d'écheance", max_length=100)
