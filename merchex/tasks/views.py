36e08b7230214503dd74528d5459b537a6cdaf1e
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-03-18 11:25:37 +0000

    update

--- merchex/tasks/views.py
+++ merchex/tasks/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse, reverse_lazy
 from datetime import datetime
 import time
 import queue
+from queue import Queue
 
-from tasks.queue_class import Queues
 from tasks.stack_class import Stack
 from .models import Task
 from .forms import TaskForm
@@ -12,47 +12,64 @@ from django.views.generic import ListView, DetailView, \
     CreateView, UpdateView, DeleteView
 
 listTask = []
-listTask_by_date = Task.objects.all().order_by("date")
+listTask = Task.objects.all().order_by("date")
 listTrier = Task.objects.all().order_by("priority")
 
 stack_task = Stack()
-queue_task = Queues()
 
-
-for item in listTask_by_date:
-    queue_task.add_task(item)
-    
 for task in listTrier[::-1]:
     stack_task.add_task(task)
+    
+
+listTask = stack_task.items
+# print(type(stack_task.items))
+    
+    # print(x.priority)
+# for task in listTrier:
+#     # print(task.priority)
+#     stack_task.add_task(task)
 
 
-listTask = stack_task.get_all()
 
 
-
-
+listObjet = []
 
     
 
 print('--------------------------')
+# for task in listTrier:
+#     print(task.date)
+for task in  listTask:
+    objectTask = {}
+    objectTask["id"]= task.id
+    objectTask["title"]= task.title
+    objectTask["description"]= task.description
+    objectTask["category"]= task.category
+    objectTask["date"]= task.date
+    objectTask["priority"]= task.priority
+    listObjet.append(objectTask)
+    
 
-for task in  stack_task.get_all():
-    print(task.priority)
 
+
+for task in listTask:
+      print(task)
+    
     
 print('--------------------------')
 
-
-for task in  queue_task.get_all():
-    print(task.date)
-    
-print('--------------------------')
 
 class TaskListView(ListView):
     model = Task
     context_object_name = 'listTask'
     
-
+# class TaskListView(ListView):
+#     model = Task
+#     context_object_name = 'listTask'
+#     queryset = stack_task.get_all()
+#     template_name = "tasks/task_list.html"
+    
+    
 class TaskDetailView(DetailView):
     model = Task
 
